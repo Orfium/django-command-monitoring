fe43ee17f2d27c69fb59d936ca818b9eeee89d74
siakon <kostas@orfium.com>
2018-10-12 18:23:02 +0300

    1.12 version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     name="django_command_monitor",
 
     # Version number
-    version="0.1.11",
+    version="0.1.12",
 
     # Application author details
     author="Konstantinos Siaterlis",
